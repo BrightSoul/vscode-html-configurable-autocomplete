255d42e0b9895571754ab1931307a9afa9588716
Moreno Gentili <brightsoul@tiscali.it>
2020-08-15 17:00:32 +0200

    Fix tabs/spaces for standard style

--- models/providers/ConfigurableCompletionItemProvider.js
+++ models/providers/ConfigurableCompletionItemProvider.js
@@ -4,133 +4,133 @@ const PathProvider = require('../services/PathProvider')
 const Logger = require('../services/Logger')
 
 module.exports = class ConfigurableCompletionItemProvider {
-	/**
+  /**
    * @type {import('../options/ConfigurableCompletionItemOptions')}
    */
-	options;
+  options;
 
-	/**
+  /**
    * @type {import('../services/ProviderRegistry')}
    */
-	providerRegistry;
+  providerRegistry;
 
-	/**
+  /**
    *
    * @param {import('../options/ConfigurableCompletionItemOptions')} options
    * @param {import('../services/ProviderRegistry')} providerRegistry
    */
-	constructor (options, providerRegistry) {
-	  this.options = options
-	  this.providerRegistry = providerRegistry
-	}
+  constructor (options, providerRegistry) {
+    this.options = options
+    this.providerRegistry = providerRegistry
+  }
 
-	/**
+  /**
    * @param {vscode.TextDocument} document
    * @param {vscode.Position} position
    * @param {vscode.CancellationToken} token
    * @returns {Promise<vscode.CompletionList|undefined>}
    */
-	async provideCompletionItems (document, position, token) {
-	  if (!this.shouldPerformCompletion(document, position)) {
-	    return
-	  }
+  async provideCompletionItems (document, position, token) {
+    if (!this.shouldPerformCompletion(document, position)) {
+      return
+    }
 
-	  const staticCompletionItems = this.getStaticCompletionItems()
-	  const fileCompletionItems = await this.findCompletionItemsInFiles(document, position, token)
+    const staticCompletionItems = this.getStaticCompletionItems()
+    const fileCompletionItems = await this.findCompletionItemsInFiles(document, position, token)
 
-	  if (token.isCancellationRequested) {
-	    return
-	  }
-	  return new vscode.CompletionList([...staticCompletionItems, ...fileCompletionItems])
-	}
+    if (token.isCancellationRequested) {
+      return
+    }
+    return new vscode.CompletionList([...staticCompletionItems, ...fileCompletionItems])
+  }
 
-	/**
+  /**
    * @param {vscode.TextDocument} document
    * @param {vscode.Position} position
    * @returns {boolean}
    */
-	shouldPerformCompletion (document, position) {
-	  const currentLine = document.lineAt(position.line)
-	  const currentCharacter = currentLine.text.charAt(position.character - 1)
-	  if (!this.options.triggerCharacters.includes(currentCharacter)) {
-	    // Skip because the last typed character is not one on the trigger characters
-	    return false
-	  }
+  shouldPerformCompletion (document, position) {
+    const currentLine = document.lineAt(position.line)
+    const currentCharacter = currentLine.text.charAt(position.character - 1)
+    if (!this.options.triggerCharacters.includes(currentCharacter)) {
+      // Skip because the last typed character is not one on the trigger characters
+      return false
+    }
 
-	  if (!this.options.triggerRegexp) {
-	    // No filtering provided, go ahead with completion
-	    return true
-	  }
+    if (!this.options.triggerRegexp) {
+      // No filtering provided, go ahead with completion
+      return true
+    }
 
-	  const regexp = new RegExp(this.options.triggerRegexp.source, this.options.triggerRegexp.flags)
-	  const relevantText = currentLine.text.substr(0, position.character)
-	  let match
-	  while ((match = regexp.exec(relevantText))) {
-	    if (position.character >= match.index && position.character <= (match.index + match[0].length)) {
-	      // The cursor is inside a match, let's go ahead with completion
-	      return true
-	    }
-	  }
+    const regexp = new RegExp(this.options.triggerRegexp.source, this.options.triggerRegexp.flags)
+    const relevantText = currentLine.text.substr(0, position.character)
+    let match
+    while ((match = regexp.exec(relevantText))) {
+      if (position.character >= match.index && position.character <= (match.index + match[0].length)) {
+        // The cursor is inside a match, let's go ahead with completion
+        return true
+      }
+    }
 
-	  return false
-	}
+    return false
+  }
 
-	/**
+  /**
    * @return {Array<vscode.CompletionItem>}
    */
-	getStaticCompletionItems () {
-	  return this.options.staticItems.map(item => new vscode.CompletionItem(item, this.options.itemKind))
-	}
+  getStaticCompletionItems () {
+    return this.options.staticItems.map(item => new vscode.CompletionItem(item, this.options.itemKind))
+  }
 
-	/**
+  /**
    * @param {vscode.TextDocument} document
    * @param {vscode.Position} position
    * @param {vscode.CancellationToken} token
    * @return {Promise<Array<vscode.CompletionItem>>}
    */
-	async findCompletionItemsInFiles (document, position, token) {
-	  if (!this.options.includeGlobPattern || !this.options.contentRegexp) {
-	    return []
-	  }
+  async findCompletionItemsInFiles (document, position, token) {
+    if (!this.options.includeGlobPattern || !this.options.contentRegexp) {
+      return []
+    }
 
-	  const includeGlobPattern = await PathProvider.replaceAllPathVariables(this.options.includeGlobPattern, this.providerRegistry, document, position, token)
-	  const excludeGlobPattern = await PathProvider.replaceAllPathVariables(this.options.excludeGlobPattern, this.providerRegistry, document, position, token)
+    const includeGlobPattern = await PathProvider.replaceAllPathVariables(this.options.includeGlobPattern, this.providerRegistry, document, position, token)
+    const excludeGlobPattern = await PathProvider.replaceAllPathVariables(this.options.excludeGlobPattern, this.providerRegistry, document, position, token)
 
-	  if (!includeGlobPattern) {
-	    return []
-	  }
+    if (!includeGlobPattern) {
+      return []
+    }
 
-	  const results = await vscode.workspace.findFiles(includeGlobPattern, excludeGlobPattern, this.options.maxFiles, token)
-	  /**
-	   * @type {Array<vscode.CompletionItem>}
-	   */
-	  const completionList = []
+    const results = await vscode.workspace.findFiles(includeGlobPattern, excludeGlobPattern, this.options.maxFiles, token)
+    /**
+     * @type {Array<vscode.CompletionItem>}
+     */
+    const completionList = []
 
-	  if (results.length === 0) {
-	    Logger.warn(`Couldn't find any file for include pattern ${includeGlobPattern} and exclude pattern ${excludeGlobPattern} in completion item rule with trigger characters ${this.options.triggerCharacters.join(',')}`)
-	    return completionList
-	  }
+    if (results.length === 0) {
+      Logger.warn(`Couldn't find any file for include pattern ${includeGlobPattern} and exclude pattern ${excludeGlobPattern} in completion item rule with trigger characters ${this.options.triggerCharacters.join(',')}`)
+      return completionList
+    }
 
-	  for (const result of results) {
-	    try {
-	      if (token.isCancellationRequested) {
-	        break
-	      }
-	      const content = await fs.readFile(result.fsPath)
-	      const contentText = content.toString()
+    for (const result of results) {
+      try {
+        if (token.isCancellationRequested) {
+          break
+        }
+        const content = await fs.readFile(result.fsPath)
+        const contentText = content.toString()
 
-	      let itemPerFileMaxCount = this.options.maxItemsPerFile
-	      let match
-	      const regexp = new RegExp(this.options.contentRegexp.source, this.options.contentRegexp.flags)
-	      while ((match = regexp.exec(contentText)) && (itemPerFileMaxCount-- > 0) && (completionList.length <= this.options.maxItems)) {
-	        const itemText = match[1] || match[0]
-	        const item = new vscode.CompletionItem(itemText, this.options.itemKind)
-	        completionList.push(item)
-	      }
-	    } catch (error) {
-	      Logger.error(error)
-	    }
-	  }
-	  return completionList
-	}
+        let itemPerFileMaxCount = this.options.maxItemsPerFile
+        let match
+        const regexp = new RegExp(this.options.contentRegexp.source, this.options.contentRegexp.flags)
+        while ((match = regexp.exec(contentText)) && (itemPerFileMaxCount-- > 0) && (completionList.length <= this.options.maxItems)) {
+          const itemText = match[1] || match[0]
+          const item = new vscode.CompletionItem(itemText, this.options.itemKind)
+          completionList.push(item)
+        }
+      } catch (error) {
+        Logger.error(error)
+      }
+    }
+    return completionList
+  }
 }
--- models/providers/ConfigurableDefinitionProvider.js
+++ models/providers/ConfigurableDefinitionProvider.js
@@ -4,93 +4,93 @@ const Logger = require('../services/Logger')
 const fs = require('fs').promises
 
 module.exports = class ConfigurableDefinitionProvider {
-	/**
+  /**
    * @type {import('../options/ConfigurableDefinitionOptions')}
    */
-	options;
+  options;
 
-	/**
+  /**
    * @param {import('../options/ConfigurableDefinitionOptions')} options
    */
-	constructor (options) {
-	  this.options = options
-	}
+  constructor (options) {
+    this.options = options
+  }
 
-	/**
+  /**
    * @param {vscode.TextDocument} document
    * @param {vscode.Position} position
    * @param {vscode.CancellationToken} token
    * @returns {Promise<vscode.Location|undefined>}
    */
-	async provideDefinition (document, position, token) {
-	  const definitionName = this.getDefinitionName(document, position)
-	  if (!definitionName) {
-	    // Couldn't extract a definition name from current location, just return
-	    return
-	  }
+  async provideDefinition (document, position, token) {
+    const definitionName = this.getDefinitionName(document, position)
+    if (!definitionName) {
+      // Couldn't extract a definition name from current location, just return
+      return
+    }
 
-	  const definitionResult = await this.findDefinitionInFiles(definitionName, document, token)
-	  return definitionResult
-	}
+    const definitionResult = await this.findDefinitionInFiles(definitionName, document, token)
+    return definitionResult
+  }
 
-	/**
+  /**
    * @param {vscode.TextDocument} document
    * @param {vscode.Position} position
    * @returns {string|undefined}
    */
-	getDefinitionName (document, position) {
-	  const currentLine = document.lineAt(position.line)
-	  let match
-	  const regexp = new RegExp(this.options.definitionRegexp.source, this.options.definitionRegexp.flags)
-	  while ((match = regexp.exec(currentLine.text))) {
-	    if (position.character >= match.index && position.character <= match.index + match[0].length) {
-	      const definitionName = match[1] || match[0]
-	      return definitionName
-	    }
-	  }
-	}
+  getDefinitionName (document, position) {
+    const currentLine = document.lineAt(position.line)
+    let match
+    const regexp = new RegExp(this.options.definitionRegexp.source, this.options.definitionRegexp.flags)
+    while ((match = regexp.exec(currentLine.text))) {
+      if (position.character >= match.index && position.character <= match.index + match[0].length) {
+        const definitionName = match[1] || match[0]
+        return definitionName
+      }
+    }
+  }
 
-	/**
+  /**
    *
    * @param {string} definitionName
    * @param {vscode.TextDocument} document
    * @param {vscode.CancellationToken} token
    * @returns {Promise<vscode.Location|undefined>}
    */
-	async findDefinitionInFiles (definitionName, document, token) {
-	  const includeGlobPattern = PathProvider.replaceDocumentPathVariables(this.options.includeGlobPattern, document)
-	  const excludeGlobPattern = PathProvider.replaceDocumentPathVariables(this.options.excludeGlobPattern, document)
-	  if (!includeGlobPattern) {
-	    return
-	  }
-	  const results = await vscode.workspace.findFiles(includeGlobPattern, excludeGlobPattern, undefined, token)
+  async findDefinitionInFiles (definitionName, document, token) {
+    const includeGlobPattern = PathProvider.replaceDocumentPathVariables(this.options.includeGlobPattern, document)
+    const excludeGlobPattern = PathProvider.replaceDocumentPathVariables(this.options.excludeGlobPattern, document)
+    if (!includeGlobPattern) {
+      return
+    }
+    const results = await vscode.workspace.findFiles(includeGlobPattern, excludeGlobPattern, undefined, token)
 
-	  if (results.length === 0) {
-	    Logger.warn(`Couldn't find any file for include pattern ${includeGlobPattern} and exclude pattern ${excludeGlobPattern} in definition provider rule with definition regexp ${this.options.definitionRegexp}`)
-	    return
-	  }
+    if (results.length === 0) {
+      Logger.warn(`Couldn't find any file for include pattern ${includeGlobPattern} and exclude pattern ${excludeGlobPattern} in definition provider rule with definition regexp ${this.options.definitionRegexp}`)
+      return
+    }
 
-	  for (const result of results) {
-	    if (token.isCancellationRequested) {
-	      return
-	    }
-	    const content = await fs.readFile(result.fsPath)
-	    const textContent = content.toString()
-	    const regexp = new RegExp(this.options.contentRegexp.source, this.options.contentRegexp.flags)
-	    let match
-	    while ((match = regexp.exec(textContent))) {
-	      const resultName = match[1] || match[0]
-	      if (resultName !== definitionName) {
-	        continue
-	      }
-	      // What line? What character?
-	      const lines = textContent.substr(0, match.index).split('\n')
+    for (const result of results) {
+      if (token.isCancellationRequested) {
+        return
+      }
+      const content = await fs.readFile(result.fsPath)
+      const textContent = content.toString()
+      const regexp = new RegExp(this.options.contentRegexp.source, this.options.contentRegexp.flags)
+      let match
+      while ((match = regexp.exec(textContent))) {
+        const resultName = match[1] || match[0]
+        if (resultName !== definitionName) {
+          continue
+        }
+        // What line? What character?
+        const lines = textContent.substr(0, match.index).split('\n')
 
-	      const uri = vscode.Uri.file(result.fsPath)
-	      const position = new vscode.Position(lines.length - 1, lines[lines.length - 1].length)
-	      const location = new vscode.Location(uri, position)
-	      return location
-	    }
-	  }
-	}
+        const uri = vscode.Uri.file(result.fsPath)
+        const position = new vscode.Position(lines.length - 1, lines[lines.length - 1].length)
+        const location = new vscode.Location(uri, position)
+        return location
+      }
+    }
+  }
 }
